16e5b40d803bd5a6887ed699d38196b0cc2e72e0
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-07-25 16:38:14 +0530

    Output Data binding

--- AngularBasics/src/app/child/child.component.ts
+++ AngularBasics/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -10,4 +10,9 @@ export class ChildComponent {
 
   @Input() name: string = '';
 
+  @Output() nameChange = new EventEmitter<string>();
+  
+  changeName() {
+    this.nameChange.emit('Nithya');
+  }
 }
--- AngularBasics/src/app/parent/parent.component.ts
+++ AngularBasics/src/app/parent/parent.component.ts
@@ -4,9 +4,14 @@ import { ChildComponent } from '../child/child.component';
 @Component({
   selector: 'app-parent',
   imports: [ChildComponent],
-  template: `<app-child [name] = '"Raghav"'></app-child>`,
+  template: `<app-child [name] = '"Raghav"' (nameChange) = receiveUpdatedName($event)></app-child>
+            {{changeName}}`,
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
-
+  changeName: string = '';
+receiveUpdatedName(name: string) {
+    console.log(`Received updated name from child: ${name}`);
+    this.changeName = name;
+  }
 }
