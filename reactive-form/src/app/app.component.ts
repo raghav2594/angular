8410b9d778107cc7ef72192b4996364f17bede47
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-09-17 16:49:04 +0530

    Reactive-form   * Basic implementation is ready

---
+++ reactive-form/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public personForm!: FormGroup;
+
+  ngOnInit() {
+    this.personForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl('Raju'),
+      email: new FormControl(),
+      gender: new FormControl(),
+      address: new FormGroup({
+        street: new FormControl(),
+        city: new FormControl(),
+        country: new FormControl()
+      })
+    });
+  }
+
+  onSubmit() {
+    console.log(this.personForm.value);
+  }
+}
